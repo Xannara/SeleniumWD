9df9bd6414cfdcef16c6344578ad93c16b650019
Xannara <30448299+Xannara@users.noreply.github.com>
2020-07-05 02:03:34 +0400

    Подсчет стикеров

---
+++ src/test/java/CheckSticker.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class CheckSticker extends ApplicationManager {
+
+    @Test
+    public void checkStickerTest() {
+        driver.get("http://localhost/litecart/");
+
+        List<WebElement> goods = driver.findElements(By.className("product"));
+        int countGoods = driver.findElements(By.className("sticker")).size();
+
+        for (int i = 0; i < goods.size() ; i++){
+            assertEquals(countGoods, goods.size());
+        }
+    }
+}
+
