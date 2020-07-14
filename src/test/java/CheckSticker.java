edf5010134703f0c417de1635b502d6cdd27fd6d
Xannara <30448299+Xannara@users.noreply.github.com>
2020-07-15 03:24:14 +0400

    Получение кол-ва стикеров у товара следующая попытка

--- src/test/java/CheckSticker.java
+++ src/test/java/CheckSticker.java
@@ -1,10 +1,12 @@
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
-import static org.junit.Assert.assertTrue;
+import static org.openqa.selenium.By.className;
+import static org.openqa.selenium.By.xpath;
 
 public class CheckSticker extends ApplicationManager {
 
@@ -12,22 +14,18 @@ public class CheckSticker extends ApplicationManager {
     public void checkStickerTest() {
         driver.get("http://localhost/litecart/");
 
-        List<WebElement> goods = driver.findElements(By.className("product"));
+        List<WebElement> goods = driver.findElements(className("product"));
         for (int i = 0; i < goods.size(); i++) {
-            goods = driver.findElements(By.className("product"));
-            goods.get(i).click();
-            assertTrue(isElementPresent(By.cssSelector("#box-product")));
+            goods = driver.findElements(className("product"));
             List<WebElement> stickers;
-            stickers = driver.findElements(By.xpath("//div[@id='box-product']//div[contains(@class, 'sticker')]"));
+            stickers = driver.findElements((By) className("product").findElements((SearchContext) xpath(".//div[contains(@class, 'sticker')]")));
+
             for (int j = 0; j < stickers.size(); j++) {
                 int sticker_count = stickers.size();
                 if (sticker_count == 1) {
                     System.out.println("У товара один стикер");
                 }
             }
-            WebElement link;
-            link = driver.findElement(By.linkText("Home"));
-            link.click();
         }
     }
 }
