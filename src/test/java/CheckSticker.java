021acb604be0b9a598daef5ad8d6ab4c95e78749
Xannara <30448299+Xannara@users.noreply.github.com>
2020-07-17 01:50:36 +0400

    Получение кол-ва стикеров у каждого товара с помощью foreach

--- src/test/java/CheckSticker.java
+++ src/test/java/CheckSticker.java
@@ -1,6 +1,5 @@
+import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
@@ -15,19 +14,10 @@ public class CheckSticker extends ApplicationManager {
         driver.get("http://localhost/litecart/");
 
         List<WebElement> goods = driver.findElements(className("product"));
-        for (int i = 0; i < goods.size(); i++) {
-            goods.get(i);
-            WebElement good;
-            good = driver.findElement(className("product"));
-            List<WebElement> stickers;
-            stickers = good.findElements(By.xpath(".//div[contains(@class, 'sticker')]"));
-
-            for (int j = 0; j < stickers.size(); j++) {
-                int sticker_count = stickers.size();
-                if (sticker_count == 1) {
-                    System.out.println("1 sticker");
-                }
-            }
+        for (WebElement good : goods) {
+            List<WebElement> stickers = good.findElements(xpath(".//div[contains(@class, 'sticker')]"));
+            int sticker_count = stickers.size();
+            Assert.assertEquals(1, sticker_count);
         }
     }
 }
